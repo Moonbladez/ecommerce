032dcb3e20fb00ed9a7c697bb5c593cb6eaf85bd
Bekki Young <moonbladez@hotmail.com>
2020-04-02 14:05:44 +0200

    parsing form data added

--- index.js
+++ index.js
@@ -1,21 +1,34 @@
 const express = require("express");
+const bodyParser = require("body-parser")
 
 const app = express();
 
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
+
 app.get("/", (req, res) => {
-	res.send(`
+    res.send(`
         <div>
-            <form>
-                <input placeholder="email"/>
-                <input placeholder="password"/>
-                <input placeholder="password confirmation"/>
+            <form method="POST">
+                <input placeholder="email" name="email"/>
+                <input placeholder="password" name="password"/>
+                <input placeholder="password confirmation" name="passwordConfirmation"/>
                 <button>Sign Up</button>
             </form>
         </div>
     `);
 });
 
+
+
+app.post("/", (req, res) => {
+    console.log(req.body)
+    //get access to email, pw
+    res.send("Account Created");
+});
+
 //listen for network requests(on port 3000)
 app.listen(3000, () => {
-	console.log("listening");
+    console.log("listening");
 });
